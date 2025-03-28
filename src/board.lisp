11b2fabffd5a36e95b166db93f32c6f1fa3db291
Mario Calcagno <mario.calcagno@protonmail.com>
2025-03-28 15:28:01 +0100

    Aggiunto i valori dei pezzi e i bonus delle posizioni

--- src/board.lisp
+++ src/board.lisp
@@ -1,15 +1,81 @@
 (in-package #:alphaknight)
 
-;;   A B C D E F G H
-;; 8 R B N Q K N B R
-;; 7 P P P P P P P P
-;; 6
-;; 5
-;; 4
-;; 3
-;; 2 P P P P P P P P
-;; 1 R B N Q K N B R   
+(defvar piece-values
+  '((:pawn   100)
+    (:knight 320)
+    (:bishop 330)
+    (:rook   500)
+    (:queen  900)
+    (:king   20000)))
+;; per ottenere il valore di un dato pezzo:
+;; (cadr (assoc :pezzo piece-values))
 
+(defstruct square-tables
+  Pawns-values
+  Knights-values
+  Bishops-valuse
+  Rooks-values
+  Queens-values
+  Kings-midgame-values
+  Kings-endgame-values)
+
+(defun init-square-tables (st)
+  (setf (square-tables-Pawns-values st) '(0  0  0   0   0   0   0  0
+					  5 10 10 -20 -20  10  10  5
+					  5 -5 -10  0   0 -10  -5  5
+					  0  0   0 20  20   0   0  0
+					  5  5  10 25  25  10   5  5
+					  10 10 20 30  30  20  10 10
+					  50 50 50 50  50  50  50 50
+					  0  0  0  0   0   0   0  0))
+  (setf (square-tables-Rooks-values st) '( 0 0 0 5 5 0 0  0
+					  -5 0 0 0 0 0 0 -5
+					  -5 0 0 0 0 0 0 -5
+					  -5 0 0 0 0 0 0 -5
+					  -5 0 0 0 0 0 0 -5
+					  -5 0 0 0 0 0 0 -5
+					  5 10 10 10 10 10 10 5
+					  0 0 0 0 0 0 0 0))
+  (setf (square-tables-Bishops-values st) '(-20 -10 -10 -10 -10 -10 -10 -20
+					    -10 5 0 0 0 0 5 -10
+					    -10 10 10 10 10 10 10 -10
+					    -10 0 10 10 10 10 0 -10
+					    -10 5 5 10 10 5 5 -10
+					    -10 0 5 10 10 5 0 -10
+					    -10 0 0 0 0 0 0 -10
+					    -20 -10 -10 -10 -10 -10 -10 -20))
+  (setf (square-tables-Knights-values st) '(-50 -40 -30 -30 -30 -30 -40 -50
+					    -40 -20 0 5 5 0 -20 -40
+					    -30 5 10 15 15 10 5 -30
+					    -30 0 15 20 20 15 0 -30
+					    -30 5 15 20 20 15 5 -30
+					    -30 0 10 15 15 10 0 -30
+					    -40 -20 0 0 0 0 -20 -40
+					    -50 -40 -30 -30 -30 -30 -40 -50))
+  (setf (square-tables-Queens-values st) '(-20 -10 -10 -5 -5 -10 -10 -20
+					   -10 0 0 0 0 5 0 -10
+					   -10 0 5 5 5 5 5 -10
+					   -5 0 5 5 5 5 0 0
+					   -5 0 5 5 5 5 0 -5
+					   -10 0 5 5 5 5 0 -10
+					   -10 0 0 0 0 0 0 -10
+					   -20 -10 -10 -5 -5 -10 -10 -20))
+  (setf (square-tables-Kings-midgame-values st) '(20 30 10 0 0 10 30 20
+						  20 20 0 0 0 0 20 20
+						  -10 -20 -20 -20 -20 -20 -20 -10
+						  -20 -30 -30 -40 -40 -30 -30 -20
+						  -30 -40 -40 -50 -50 -40 -40 -30
+						  -30 -40 -40 -50 -50 -40 -40 -30
+						  -30 -40 -40 -50 -50 -40 -40 -30
+						  -30 -40 -40 -50 -50 -40 -40 -30))
+  (setf (square-tables-Kings-lategame-values st) '(-50 -30 -30 -30 -30 -30 -30 -50
+						   -30 -30 0 0 0 0 -30 -30
+						   -30 -10 20 30 30 20 -10 -30
+						   -30 -10 30 40 40 30 -10 -30
+						   -30 -10 30 40 40 30 -10 -30
+						   -30 -10 20 30 30 20 -10 -30
+						   -30 -20 -10 0 0 -10 -20 -30
+						   -50 -40 -30 -20 -20 -30 -40 -50)))
 
 ;; Board, struct
 ;;; defines:
@@ -61,13 +127,3 @@
   (init-knights b)
   (init-queens b)
   (init-rooks b))
-
-
-;; (defun pawns->letter (b)
-;;   (dolist #'(lambda (x)
-;; 	      (cond ((= 1 x) "P")
-;; 		    ((t 'nil))))))
-
-;; (defun print-board (b)
-;;   (let ((to-print '()))
-;;     ))
