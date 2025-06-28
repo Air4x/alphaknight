2d08400ef3392106bbf43c4240a39e13c2ab6b3f
Mario Calcagno <mario.calcagno@protonmail.com>
2025-06-28 12:59:30 +0200

    Aggiunte funzioni di accesso per square-tables

--- src/evaluation.lisp
+++ src/evaluation.lisp
@@ -68,34 +68,84 @@
 						  -30 -40 -40 -50 -50 -40 -40 -30
 						  -30 -40 -40 -50 -50 -40 -40 -30
 						  -30 -40 -40 -50 -50 -40 -40 -30))
-  (setf (square-tables-Kings-lategame-values st) '(-50 -30 -30 -30 -30 -30 -30 -50
+  (setf (square-tables-Kings-endgame-values st) '(-50 -30 -30 -30 -30 -30 -30 -50
 						   -30 -30 0 0 0 0 -30 -30
 						   -30 -10 20 30 30 20 -10 -30
 						   -30 -10 30 40 40 30 -10 -30
 						   -30 -10 30 40 40 30 -10 -30
 						   -30 -10 20 30 30 20 -10 -30
 						   -30 -20 -10 0 0 -10 -20 -30
-						   -50 -40 -30 -20 -20 -30 -40 -50)))
+						   -50 -40 -30 -20 -20 -30 -40 -50))
+  st)
 
-(defun valuta-posizione (posizione piece-values square-tables)
+(defun get-square-table (type color endgame-p st)
+  (if (eql color :bianco)
+      (case type
+	(:pawn (square-tables-Pawns-values st))
+	(:rook (square-tables-Rooks-values st))
+	(:bishop (square-tables-Bishops-values st))
+	(:knight (square-tables-Knights-values st))
+	(:queen  (square-tables-Queens-values st))
+	(:king   (if endgame-p
+		     (square-tables-Kings-endgame-values st)
+		     (square-tables-Kings-midgame-values st))))
+     (case type
+	(:pawn (reverse (square-tables-Pawns-values st)))
+	(:rook (reverse (square-tables-Rooks-values st)))
+	(:bishop (reverse (square-tables-Bishops-values st)))
+	(:knight (reverse (square-tables-Knights-values st)))
+	(:queen  (reverse (square-tables-Queens-values st)))
+	(:king   (if endgame-p
+		     (reverse (square-tables-Kings-endgame-values st))
+		     (reverse (square-tables-Kings-midgame-values st)))))))
+
+(defun endgame-p (position)
+  (let ((white-queens (Board-WQueens (Posizione-board position)))
+	(black-queens (Board-BQueens (Posizione-board position)))
+	(zeroes #*0000000000000000000000000000000000000000000000000000000000000000))
+    (if (and (equal white-queens zeroes) (equal black-queens zeroes))
+	t
+	nil)))
+
+(defun valuta-posizione (posizione values square-tables)
   "Data una posizione, la valuta in relazione al giocatore che
   deve giocate ed utilizza i valori dei pezzi e la loro posizione
   per ottenere il punteggio, che viene restituito"
   (let ((score 0)
-	(pezzi (:pawn :knight :bishop :rook :queen :king))
+	(pezzi '(:pawn :knight :bishop :rook :queen :king))
 	(giocatore (Posizione-giocatore posizione)))
     (dolist (pezzo pezzi)
-      (let* ((bianco (get-White-piece pezzo posizione))
-	     (nero (get-Black-piece pezzo posizione))
-	     (valore-pezzo (cadr (assoc piece-values pezzo))))
+      (let* ((bianco (get-White-piece pezzo (Posizione-board posizione)))
+	     (valore-pezzo (cadr (assoc pezzo values)))
+	     (square-table (get-square-table pezzo giocatore (endgame-p posizione) square-tables)))
 	(dotimes (i 64)
 	  (when (= 1 (elt bianco i))
 	    ;; Se il giocatore è nero inverti l'indice della square table
 	    (let ((indice (if (eq giocatore :nero) (- 63 i) i)))
 	      (incf score valore-pezzo)
-	      (incf score (nth indice square-table)))
-	    (if (eq giocatore :nero)
-		(- score)
-		score)
-	    ))))))
+	      (incf score (nth indice square-table)))))))
+	(if (eq giocatore :nero)
+	    (- score)
+	    score)))
 
+(defun generate-legal-moves (position)
+  "Generate all legal moves with king safety check"
+  (remove-if #'(lambda (move)
+               (let ((new-pos (apply-move position move)))
+                 (king-in-check-p new-pos)))
+             (generate-pseudo-legal-moves position)))
+
+(defun terminalp (position)
+  "Check if position is terminal (checkmate/stalemate)"
+  (let ((legal-moves (generate-legal-moves position)))
+    (cond
+      ((king-in-check-p position)
+       (if (null legal-moves) :checkmate nil))
+      ((null legal-moves) :stalemate)
+      (t nil))))
+
+
+
+
+(defvar *square-tables* (make-square-tables))
+(init-square-tables *square-tables*)
