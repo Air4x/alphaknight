dc3e82dd049fc8bed1051b5da07d7d7bb413b7c1
Mario Calcagno <mario.calcagno@protonmail.com>
2025-06-28 13:00:56 +0200

    Creato il main loop per la simulazione

--- src/main.lisp
+++ src/main.lisp
@@ -1,4 +1,13 @@
-(in-package #:alphaknight)
+(in-package :alphaknight)
+
+(defvar *position* (make-Posizione))
+(init-Posizione *position*)
 
 (defun start ()
-  (format t "Hello World"))
+  (format t "Welcome to Alphaknight, the simulation will begin...~%")
+  (loop
+   (print-board (Posizione-board *position*))
+   (let ((best-move (find-best-move *position* 3)))
+     (setf *position* (apply-move *position* best-move))
+     (print-board (Posizione-board *position*))
+     (terpri))))
